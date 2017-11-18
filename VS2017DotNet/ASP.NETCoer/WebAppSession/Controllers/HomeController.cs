c28b79cd3c48c5f6c12f23287f592e6d8d958a7a
胡睿毅 <807776962@qq.com>
2017-11-18 20:33:58 +0800

    X509Store

---
+++ VS2017DotNet/ASP.NETCoer/WebAppSession/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebAppSession.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string SessionKeyName = "_Name";
+        private const string SessionKeyYearsMember = "_YearsMember";
+        private const string SessionKeyDate = "_Date";
+
+        public IActionResult Index()
+        {
+            // Requires using Microsoft.AspNetCore.Http;
+            HttpContext.Session.SetString(SessionKeyName, "Rick");
+            HttpContext.Session.SetInt32(SessionKeyYearsMember, 3);
+            return RedirectToAction("SessionNameYears");
+        }
+
+        public IActionResult SessionNameYears()
+        {
+            string name = HttpContext.Session.GetString(SessionKeyName);
+            int? yearsMember = HttpContext.Session.GetInt32(SessionKeyYearsMember);
+
+            return Content($"Name: \"{name}\",  Membership years: \"{yearsMember}\"");
+        }
+
+        public IActionResult SetDate()
+        {
+            // Requires you add the Set extension method mentioned in the article.
+            HttpContext.Session.Set<DateTime>(SessionKeyDate, DateTime.Now);
+            return RedirectToAction("GetDate");
+        }
+
+        public IActionResult GetDate()
+        {
+            // Requires you add the Get extension method mentioned in the article.
+            DateTime date = HttpContext.Session.Get<DateTime>(SessionKeyDate);
+            string sessionTime = date.TimeOfDay.ToString();
+            string currentTime = DateTime.Now.TimeOfDay.ToString();
+
+            return Content($"Current time: {currentTime} - session time: {sessionTime}");
+        }
+    }
+}
