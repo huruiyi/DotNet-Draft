233eff69c477b80a7164e0b22316f1bf24a67255
胡睿毅 <807776962@qq.com>
2018-03-27 00:45:45 +0800

    AppLucene

---
+++ VS2017DotNet/WebAppLucene/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppLucene.MvcApplication" Language="C#" %>
---
+++ VS2017DotNet/WebAppLucene/Models/Article.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebAppLucene.Models
+{
+    public class Article
+    {
+        public string Id { get; set; }
+        public string ClassId { get; set; }
+        public string ClassName { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Score { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
