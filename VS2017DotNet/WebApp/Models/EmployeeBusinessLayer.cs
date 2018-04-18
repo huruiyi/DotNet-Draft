a55e48bd676909d98962186a162c8a456fd0722e
胡睿毅 <807776962@qq.com>
2018-04-18 14:08:32 +0800

    WebApp

---
+++ VS2017DotNet/WebApp/Models/EmployeeBusinessLayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApp.Models
+{
+    public class EmployeeBusinessLayer
+    {
+        public List<Employee> GetEmployees()
+        {
+            SalesERPDAL salesDal = new SalesERPDAL();
+            return salesDal.Employees.ToList();
+        }
+
+        public Employee SaveEmployee(Employee e)
+        {
+            SalesERPDAL salesDal = new SalesERPDAL();
+            salesDal.Employees.Add(e);
+            salesDal.SaveChanges();
+            return e;
+        }
+    }
+}
