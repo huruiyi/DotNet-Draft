f70bff2845389f85c760805ae1b7388b7f10e4af
胡睿毅 <807776962@qq.com>
2018-05-05 11:25:26 +0800

    ExportToWord

---
+++ VS2017DotNet/WebApp.WebForm/Page/ExportToWord.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+
+namespace WebApp.WebForm.Page
+{
+    public partial class ExportToWord : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void Export()
+        {
+            Response.Clear();
+            Response.Buffer = true;
+            Response.Charset = "utf-8";
+
+            string fileName = HttpUtility.UrlEncode("对账单" + ".doc");
+            Response.AddHeader("Content-Disposition", "attachment;filename=" + fileName);
+            Response.ContentEncoding = Encoding.GetEncoding("utf-8");
+
+            Response.ContentType = "application/ms-word";
+            Page.EnableViewState = false;
+
+            StringWriter oStringWriter = new StringWriter();
+            HtmlTextWriter oHtmlTextWriter = new HtmlTextWriter(oStringWriter);
+
+            exportTableInfo.RenderControl(oHtmlTextWriter);
+
+            Response.Write(oStringWriter.ToString());
+            Response.End();
+        }
+    }
+}
