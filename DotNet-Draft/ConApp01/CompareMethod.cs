7fa7f618c98caf24811ea68c80dda6cf468084aa
胡睿毅 <807776962@qq.com>
2019-09-18 14:58:30 +0800

    Clean

--- DotNet-Draft/ConApp01/CompareMethod.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ConApp01
-{
-    public enum CompareMethod
-    {
-        LinqAnyBothEquals = 0,
-        Contains = 1,
-    }
-}
--- DotNet-Draft/ConApp01/Data01.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConApp01
-{
-    /// <summary>
-    /// Plain vanilla dataclass with nothing special to help
-    /// </summary>
-    public class Data01 : User
-    {
-    }
-}
